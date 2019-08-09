17659fb1d80047ad193bbae0e482890c2ae9dcdf
xieyuzhong <xieyuzhong@haizhi.com>
2019-08-09 17:28:08 +0800

    chore(ts): format ts code style

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'
-import * as fs from 'fs-extra'
-import * as path from 'path'
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as vscode from "vscode";
 
-import { createFiles } from './fileCreator'
+import { createFiles } from "./fileCreator";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand("extension.dolly", async (e: vscode.Uri) => {
+    // The code you place here will be executed every time your command is executed
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dolly" is now active!');
+    let directoryPath = (e && e.fsPath) ? e.fsPath : vscode.workspace.rootPath || "";
+    if (!(await fs.stat(directoryPath)).isDirectory()) {
+      directoryPath = path.dirname(directoryPath);
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.dolly', async (e: vscode.Uri) => {
-		// The code you place here will be executed every time your command is executed
+    try {
+      await createFiles(directoryPath);
+      vscode.window.showInformationMessage("创建成功");
+    } catch (e) {
+      vscode.window.showErrorMessage(e.message);
+    }
+  });
 
-		let directoryPath = (e && e.fsPath) ? e.fsPath : vscode.workspace.rootPath || ''
-		if (!(await fs.stat(directoryPath)).isDirectory()) {
-			directoryPath = path.dirname(directoryPath)
-		}
-		
-		try {
-			await createFiles(directoryPath)
-			vscode.window.showInformationMessage('创建成功')
-		} catch (e) {
-			vscode.window.showErrorMessage(e.message)
-			console.error(e)
-		}
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
--- src/fileCreator/createFiles.ts
+++ src/fileCreator/createFiles.ts
@@ -1,22 +1,20 @@
-import * as fs from 'fs-extra'
-import * as path from 'path'
-import * as vscode from 'vscode'
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as vscode from "vscode";
 
 export function createFiles(inDirectory: string): Promise<void> {
-  const configuration = vscode.workspace.getConfiguration('dolly')
-  const templatePath = configuration.get('templatePath') as string
-  const absTemplatePath = path.join(vscode.workspace.rootPath as string, templatePath)
-  const newFolderName = configuration.get('newFolderName') as string
+  const configuration = vscode.workspace.getConfiguration("dolly");
+  const templatePath = configuration.get("templatePath") as string;
+  const absTemplatePath = path.join(vscode.workspace.rootPath as string, templatePath);
+  const newFolderName = configuration.get("newFolderName") as string;
 
   return new Promise((resolve, reject) => {
-    fs.copy(absTemplatePath, path.join(inDirectory, newFolderName), function (err) {
+    fs.copy(absTemplatePath, path.join(inDirectory, newFolderName), (err) => {
       if (err) {
-        console.log('An error occured while copying the folder.')
-        reject(err)
-        return
+        reject(err);
+        return;
       }
-      console.log('Copy completed!')
-      resolve()
-    })
-  })
+      resolve();
+    });
+  });
 }
